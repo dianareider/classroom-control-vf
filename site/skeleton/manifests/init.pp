24f52c3c9b726b7358e41264e3f8b8bb3fa8ac3f
dianareider <dianareider@gmail.com>
2016-06-28 14:39:13 -0700

    Removed owner info for bashrc, erroring
        owner => 'root',
        group => 'root',
        mode => '0644',

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -7,8 +7,5 @@ class skeleton {
   }
   file {'/etc/skel/.bashrc':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/skeleton/bashrc'
 }
