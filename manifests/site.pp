2a74867a9585cf5009388ce2ecba0f3dc256f478
dianareider <dianareider@gmail.com>
2016-06-29 13:40:22 -0700

    Added VM detection

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,6 +43,12 @@ include users
 include skeleton
 include memcached
 include nginx
+
+if $::virtual !=physical {
+  $vmname = capitalize($::virtual)
+  notify { "This is a ${vmname} virtual machine." :}
+}
+
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
