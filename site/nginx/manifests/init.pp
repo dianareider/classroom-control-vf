8dba985dfc13eafd03646de145cc30d651b7577e
dianareider <dianareider@gmail.com>
2016-06-29 12:53:17 -0700

    Code simplification

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,40 +3,34 @@ class nginx {
     ensure => present,
   }
   
-  file {'docroot':
-    ensure => directory,
-    path => '/var/www',
+  File {
     owner => 'root',
     group => 'root',
     mode => '0775',
+  }
+  
+  file {['/var/www','/etc/ngenx/conf.d']:
+    ensure => directory,
     }
   
   file {'index.html':
     ensure => file,
     path => '/var/www/index.html',
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
 
   file {'nginx.conf':
     ensure => file,
     path => '/etc/nginx/nginx.conf',
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['ngenx'],
   }
 
   file {'default.conf':
     ensure => file,
     path => '/etc/nginx/conf.d/default.conf',
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
-    require => [Package['nginx'],File['docroot']],
+    require => Package['nginx'],
   }
   
   service {'nginx':
